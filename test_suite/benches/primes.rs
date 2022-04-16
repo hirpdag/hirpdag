e0a8583afe7e068ab8ea36087e7571bfa95b0b0b
Andrew Browne <dersaidin@dersaidin.net>
2022-04-15 22:48:43 -0700

    Cleanup primes benchmark.

---
+++ test_suite/benches/primes.rs
@@ -0,0 +1,226 @@
+macro_rules! implementation {
+    () => (
+    impl Number {
+        fn is_prime(&self) -> bool {
+            self.factors.is_empty()
+        }
+    }
+
+    fn factors(n: usize, nums: &Vec<Number>) -> Vec<Number> {
+        if n <= 3 {
+            return vec![];
+        }
+        let mut result = vec![];
+        let mut m = n;
+        while m > 3 {
+            let sqrt_m = (m as f64).sqrt() as usize;
+            let mut found = false;
+            for i in 2..=sqrt_m {
+                if (i * (m / i)) == m {
+                    result.push(nums[i].clone());
+                    m = m / i;
+                    found = true;
+                    break;
+                }
+            }
+            if !found {
+                break;
+            }
+        }
+        result
+    }
+
+    struct IncrementVBy {
+        inc: usize,
+    }
+
+    impl IncrementVBy {
+        fn new(n: usize) -> HirpdagRewriteMemoized<Self> {
+            HirpdagRewriteMemoized::new(Self { inc: n })
+        }
+    }
+
+    impl HirpdagRewriter for IncrementVBy {
+        fn rewrite_Number(&self, x: &Number) -> Number {
+            Number::new(
+                x.n,
+                self.rewrite(&x.factors),
+                self.rewrite(&x.last_prime),
+                x.v + self.inc,
+            )
+        }
+    }
+
+    fn populate_numbers_single(limit: usize, v: usize) {
+        let mut nums: Vec<Number> = vec![];
+        let mut last_prime: Option<Number> = None;
+        for n in 0..limit {
+            let a: Number = Number::new(n, factors(n, &nums), last_prime.take(), v);
+            if a.is_prime() && n > 0 {
+                last_prime = Some(a.clone());
+            }
+            nums.push(a);
+        }
+
+        // Increment rewrite
+        let t_inc = IncrementVBy::new(1);
+        let nums2 = t_inc.rewrite(&nums);
+        criterion::black_box(nums2);
+    }
+
+    pub fn populate_numbers(limit: usize, threads: usize) {
+        match threads {
+            1 => {
+                populate_numbers_single(limit, 0);
+            }
+            _ => {
+                let mut children = vec![];
+                for i in 1..=threads {
+                    children.push(std::thread::spawn(move || {
+                        populate_numbers_single(limit, i);
+                    }));
+                }
+                for c in children {
+                    let _ = c.join();
+                }
+            }
+        }
+    }
+
+    )
+}
+
+mod arc_hash_linear {
+    use hirpdag::*;
+
+    #[hirpdag]
+    struct Number {
+        n: usize,
+        factors: Vec<Number>,
+        last_prime: Option<Number>,
+        v: usize,
+    }
+
+    #[hirpdag_end(
+    reference_type = "hirpdag_hashconsing::RefArc<D>",
+    reference_weak_type = "hirpdag_hashconsing::RefArcWeak<D>",
+    table_type = "hirpdag_hashconsing::TableHashmapFallbackWeak<D, hirpdag_hashconsing::RefArc<D>, hirpdag_hashconsing::RefArcWeak<D>, hirpdag_hashconsing::TableVecLinearWeak<D, hirpdag_hashconsing::RefArc<D>, hirpdag_hashconsing::RefArcWeak<D>>>",
+    tableshared_type = "hirpdag_hashconsing::TableSharedSharded<D, hirpdag_hashconsing::RefArc<D>, ImplTable<D>>",
+    build_tableshared_type = "hirpdag_hashconsing::BuildTableSharedSharded<D, hirpdag_hashconsing::RefArc<D>, ImplTable<D>, hirpdag_hashconsing::BuildTableDefault<ImplTable<D>>, std::hash::BuildHasherDefault<std::collections::hash_map::DefaultHasher>>",
+)]
+    pub struct HirpdagEndMarker;
+
+    implementation!();
+}
+
+mod arc_hash_sorted {
+    use hirpdag::*;
+
+    #[hirpdag]
+    struct Number {
+        n: usize,
+        factors: Vec<Number>,
+        last_prime: Option<Number>,
+        v: usize,
+    }
+
+    #[hirpdag_end(
+    reference_type = "hirpdag_hashconsing::RefArc<D>",
+    reference_weak_type = "hirpdag_hashconsing::RefArcWeak<D>",
+    table_type = "hirpdag_hashconsing::TableHashmapFallbackWeak<D, hirpdag_hashconsing::RefArc<D>, hirpdag_hashconsing::RefArcWeak<D>, hirpdag_hashconsing::TableVecSortedWeak<D, hirpdag_hashconsing::RefArc<D>, hirpdag_hashconsing::RefArcWeak<D>>>",
+    tableshared_type = "hirpdag_hashconsing::TableSharedSharded<D, hirpdag_hashconsing::RefArc<D>, ImplTable<D>>",
+    build_tableshared_type = "hirpdag_hashconsing::BuildTableSharedSharded<D, hirpdag_hashconsing::RefArc<D>, ImplTable<D>, hirpdag_hashconsing::BuildTableDefault<ImplTable<D>>, std::hash::BuildHasherDefault<std::collections::hash_map::DefaultHasher>>",
+)]
+    pub struct HirpdagEndMarker;
+
+    implementation!();
+}
+
+mod arc_tovweaktable {
+    use hirpdag::*;
+
+    #[hirpdag]
+    struct Number {
+        n: usize,
+        factors: Vec<Number>,
+        last_prime: Option<Number>,
+        v: usize,
+    }
+
+    #[hirpdag_end(
+    reference_type = "hirpdag_hashconsing::RefArc<D>",
+    reference_weak_type = "hirpdag_hashconsing::RefArcWeak<D>",
+    table_type = "hirpdag_hashconsing::TableTovWeakTable<D, hirpdag_hashconsing::RefArc<D>, hirpdag_hashconsing::RefArcWeak<D>>",
+    tableshared_type = "hirpdag_hashconsing::TableSharedSharded<D, hirpdag_hashconsing::RefArc<D>, ImplTable<D>>",
+    build_tableshared_type = "hirpdag_hashconsing::BuildTableSharedSharded<D, hirpdag_hashconsing::RefArc<D>, ImplTable<D>, hirpdag_hashconsing::BuildTableDefault<ImplTable<D>>, std::hash::BuildHasherDefault<std::collections::hash_map::DefaultHasher>>",
+)]
+    pub struct HirpdagEndMarker;
+
+    implementation!();
+}
+
+mod leak_hash_linear {
+    use hirpdag::*;
+
+    #[hirpdag]
+    struct Number {
+        n: usize,
+        factors: Vec<Number>,
+        last_prime: Option<Number>,
+        v: usize,
+    }
+
+ #[hirpdag_end(
+     reference_type = "hirpdag_hashconsing::RefLeak<D>",
+     reference_weak_type = "hirpdag_hashconsing::RefLeakWeak<D>",
+     table_type = "hirpdag_hashconsing::TableHashmapFallbackWeak<D, hirpdag_hashconsing::RefLeak<D>, hirpdag_hashconsing::RefLeakWeak<D>, hirpdag_hashconsing::TableVecLinearWeak<D, hirpdag_hashconsing::RefLeak<D>, hirpdag_hashconsing::RefLeakWeak<D>>>",
+     tableshared_type = "hirpdag_hashconsing::TableSharedSharded<D, hirpdag_hashconsing::RefLeak<D>, ImplTable<D>>",
+     build_tableshared_type = "hirpdag_hashconsing::BuildTableSharedSharded<D, hirpdag_hashconsing::RefLeak<D>, ImplTable<D>, hirpdag_hashconsing::BuildTableDefault<ImplTable<D>>, std::hash::BuildHasherDefault<std::collections::hash_map::DefaultHasher>>"
+ )]
+    pub struct HirpdagEndMarker;
+
+    implementation!();
+}
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion, BenchmarkId};
+
+fn custom_criterion() -> Criterion {
+    Criterion::default().sample_size(25)
+}
+
+#[derive(Copy, Clone)]
+struct BenchPrimesParams {
+    max: usize,
+    threads: usize,
+}
+
+impl core::fmt::Display for BenchPrimesParams {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "(Nums={} Parallel={})", self.max, self.threads)
+    }
+}
+
+fn bench_primes(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Primes");
+    for t in [1, 2, 4, 8].iter() {
+        for i in [1000usize, 8000usize].iter() {
+            let params = BenchPrimesParams { max: *i, threads: *t };
+            group.bench_with_input(BenchmarkId::new("ArcHashLinear", params), &params,
+                |b, params| b.iter(|| arc_hash_linear::populate_numbers(black_box(params.max), black_box(params.threads))));
+            group.bench_with_input(BenchmarkId::new("ArcHashSorted", params), &params,
+                |b, params| b.iter(|| arc_hash_sorted::populate_numbers(black_box(params.max), black_box(params.threads))));
+            group.bench_with_input(BenchmarkId::new("ArcTovWeakTable", params), &params,
+                |b, params| b.iter(|| arc_tovweaktable::populate_numbers(black_box(params.max), black_box(params.threads))));
+            group.bench_with_input(BenchmarkId::new("LeakHashLinear", params), &params,
+                |b, params| b.iter(|| leak_hash_linear::populate_numbers(black_box(params.max), black_box(params.threads))));
+        }
+    }
+    group.finish();
+}
+
+criterion_group! {
+    name = benches;
+    config = custom_criterion();
+    targets = bench_primes
+}
+criterion_main!(benches);
